553aa64b540d81b6577b2ff5f83179bf1fa551bb
CucerOriginal <87267507+CucerOriginal@users.noreply.github.com>
2022-04-07 02:32:36 +0300

    Добавьте файлы проекта.

---
+++ DZbarsParall/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZbarsParall
+{
+    public interface IRequestHandler
+    {
+        string HandleRequest(string message, string[] arguments);
+    }
+
+    public class DummyRequestHandler : IRequestHandler
+    {
+        public string HandleRequest(string message, string[] arguments)
+        {
+            try
+            {
+                Thread.Sleep(10_000);
+                if (message.Contains("упади"))
+                {
+                    throw new Exception("Я упал, как сам просил");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Тут тотал: " + ex.Message);
+            }
+            return Guid.NewGuid().ToString("D");
+        }
+    }
+}
---
+++ DZbarsParall/Program.cs
@@ -0,0 +1,33 @@
+﻿using DZbarsParall;
+
+DummyRequestHandler dummyRequestHandler = new DummyRequestHandler();
+Console.WriteLine("Я запустил");
+
+while (true)
+{
+    Console.WriteLine("Как запрос то будет называться? Если не хочешь пиши /exit");
+    var cmd = Console.ReadLine();
+    if (cmd == "/exit")
+    {
+        Console.WriteLine("Все, я вырубаю");
+        break;
+    }
+    int count = 4;
+    string[] argSrings = new string[count];
+    Console.WriteLine("Так, а что мне отправить?");
+    string? cmd1 = "";
+    for (int i = 0; cmd1 != "/end"; i++)
+    {
+        if (count <= i)
+        {
+            Array.Resize(ref argSrings, argSrings.Length * 2);
+        }
+        cmd1 = Console.ReadLine();
+        if (cmd1 != "/end" && cmd1 != null)
+        {
+            argSrings[i] = cmd1;
+            Console.WriteLine("Что нибудь еще?");
+        }
+    }
+    new Thread(() => Console.WriteLine("тут все готово, вот пруф: " + dummyRequestHandler.HandleRequest(cmd, argSrings))).Start();
+}
